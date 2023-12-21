6bc404972d5aec647b9371adf9e96b22cf931070
Ragger <67398418+jibzdev@users.noreply.github.com>
2023-12-21 04:47:17 +0000

    mobile integration

--- Free Panel/desktop.js
+++ Free Panel/desktop.js
@@ -84,8 +84,35 @@ function timer(){
     }
 
     submitButton.addEventListener("click", () => {
-        startTimer(180);
-        localStorage.setItem('savedTime', Date.now());
+        let inputField = document.querySelector("input");
+
+        if (inputField.value === ""){
+            let error = document.createElement("p");
+            error.setAttribute("id","error");
+            error.setAttribute("class","fade");
+            error.textContent = "Invalid Link";
+            error.style.color = "red";
+            error.style.marginBottom = "-20px";
+            inputField.parentNode.insertBefore(error, inputField.nextSibling);
+            setTimeout(() => {
+                error.remove();
+            }, 2000);
+        } else{
+            let tiktok = document.querySelector("#tiktok");
+            let instagram = document.querySelector("#instagram");
+            let youtube = document.querySelector("#youtube");
+            
+            startTimer(180);
+            localStorage.setItem('savedTime', Date.now());
+            let header = document.querySelector("#header");
+            header.innerHTML = `
+            <i class="fa-solid fa-circle-check fade" style="font-size: 6vh; color: green;"></i>
+            <h1 class="fade" style="font-size: 4vh;">Request Sent</h1>`;
+
+            tiktok.addEventListener("click",redirect("tiktok"));
+            instagram.addEventListener("click",redirect("instagram"));
+            youtube.addEventListener("click",redirect("youtube"));
+        }
     });
 }
 
@@ -121,7 +148,7 @@ document.addEventListener("DOMContentLoaded", () => {
             <br><br>
             <input placeholder="Enter Link" class="fade" id="tiktokInput" name="link">
             <br><br>
-            <button id="sButton" class="fade" onClick="redirect('tiktok')">Submit</button>
+            <button id="sButton" class="fade">Submit</button>
             `;
             timer();
         });
@@ -140,7 +167,7 @@ document.addEventListener("DOMContentLoaded", () => {
             <br><br>
             <input placeholder="Enter Link" class="fade" id="instagramInput" name="link">
             <br><br>
-            <button id="sButton" class="fade" onClick="redirect('instagram')">Submit</button>
+            <button id="sButton" class="fade">Submit</button>
             `;
             timer();
         });
@@ -159,7 +186,7 @@ document.addEventListener("DOMContentLoaded", () => {
             <br><br>
             <input placeholder="Enter Link" class="fade" id="youtubeInput" name="link">
             <br><br>
-            <button id="sButton" class="fade" onClick="redirect('youtube')>Submit</button>
+            <button id="sButton" class="fade">Submit</button>
             `;
             timer();
             });
--- Free Panel/mobile.js
+++ Free Panel/mobile.js
@@ -1,15 +1,117 @@
-// function loadCSSFile() {
-//     const screenWidth = window.innerWidth;
-  
-//     if (screenWidth <= 768) {
-//       document.querySelector('#dynamic-styles').href = 'mobile.css';
-//     } else {
-//       document.querySelector('#dynamic-styles').href = 'desktop.css';
-//     }
-// }
+let timerExists = false;
+let savedTime = localStorage.getItem("savedTime");
+let timeLeft = savedTime ? Math.max(0, (180 - (Date.now() - savedTime) / 1000)) : 0;
 
-// window.addEventListener('load', loadCSSFile);
-// window.addEventListener('resize', loadCSSFile);
+function startTimer(duration) {
+    let timer = duration, minutes, seconds;
+    let display = document.querySelector('#timer');
+    display.style.display = 'block';
+    
+    let intervalId = setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            clearInterval(intervalId);
+            display.style.display = 'none';
+            localStorage.removeItem('savedTime');
+        }
+    }, 1000);
+}
+
+window.addEventListener("load", () => {
+    if (savedTime) {
+        timeLeft = Math.max(0, 180 - (Date.now() - savedTime) / 1000);
+        console.log(timeLeft);
+        if (timeLeft > 0) {
+            timerExists = true;
+            startTimer(timeLeft);
+        }
+    }
+});
+
+function redirect(platform) {
+    let dropdownValue = document.querySelector("#tools").value;
+    location.href = `/${platform}/${dropdownValue}api`;
+}
+
+
+function timer(){
+    let submitButton = document.querySelector("#sButton");
+    
+    function startTimer(duration) {
+        let timer = duration, minutes, seconds;
+        let display = document.querySelector('#timer');
+        display.style.display = 'block';
+        let intervalId = setInterval(function () {
+          minutes = parseInt(timer / 60, 10);
+          seconds = parseInt(timer % 60, 10);
+  
+          minutes = minutes < 10 ? "0" + minutes : minutes;
+          seconds = seconds < 10 ? "0" + seconds : seconds;
+  
+          display.textContent = minutes + ":" + seconds;
+  
+          if (--timer < 0) {
+            clearInterval(intervalId);
+            display.style.display = 'none';
+            submitButton.disabled = false;
+            submitButton.style.opacity = 1;
+            localStorage.removeItem('savedTime');
+          } else {
+            submitButton.disabled = true;
+            submitButton.style.opacity = 0.5;
+          }
+        }, 1000);
+      }
+      
+    if (savedTime) {
+      let timeLeft = (180000 - (Date.now() - savedTime)) / 1000;
+      console.log(timeLeft);
+      if (timeLeft > 0) {
+        submitButton.disabled = true;
+        submitButton.style.opacity = 0.5;
+        startTimer(timeLeft);
+      } else {
+        submitButton.disabled = false;
+        submitButton.style.opacity = 1;
+      }
+    }
+
+    submitButton.addEventListener("click", () => {
+        let inputField = document.querySelector("input");
+
+        if (inputField.value === ""){
+            let error = document.createElement("p");
+            error.setAttribute("id","error");
+            error.setAttribute("class","fade");
+            error.textContent = "Invalid Link";
+            error.style.color = "red";
+            error.style.marginBottom = "-20px";
+            inputField.parentNode.insertBefore(error, inputField.nextSibling);
+            setTimeout(() => {
+                error.remove();
+            }, 2000);
+        } else{
+            startTimer(180);
+            localStorage.setItem('savedTime', Date.now());
+            let header = document.querySelector("#header");
+            selectionGrid.textContent = "";
+            header.style.top = "50%";
+            header.style.transform = "translate(-50%, -50%)";
+            header.style.whiteSpace = "nowrap";
+            header.style.overflow = "hidden";
+            header.innerHTML = `
+            <i class="fa-solid fa-circle-check fade" style="font-size: 6vh; color: green;"></i>
+            <h1 class="fade" style="font-size: 4vh;">Request Sent</h1>`;
+        }
+    });
+}
 
 document.addEventListener("DOMContentLoaded", () => {
     let tiktokButton = document.querySelector("#tiktok");
@@ -21,8 +123,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
     function orignalState() {
         header.style.textShadow = "none";
-        header.textContent = "Select Service";
-        header.setAttribute("class","fade");
+        header.textContent = "Choose a Platform";
+        titleBar.setAttribute("class","fade");
         selectionGrid.style.top = "60%";
         selectionGrid.style.transform = "translate(-50%, -60%)";
         selectionGrid.setAttribute("class","fade")
@@ -31,66 +133,71 @@ document.addEventListener("DOMContentLoaded", () => {
         <a id="youtube"><i class="fa-brands fa-youtube" style="margin-right: 5px;"></i>Youtube</a>
         <a id="instagram"><i class="fa-brands fa-instagram" style="margin-right: 5px;"></i>Instagram</a>
         `;
+        addServiceListeners();
     }
     
-    tiktokButton.addEventListener("click", () => {
-        header.setAttribute("class","fade")
-        header.innerHTML = `
-        <i class="fa-brands fa-tiktok" style="font-size: 4vh"></i>
-        <h1 style="margin-top: -10px">TikTok Service</h1>`;
-        header.style.textShadow = "2px 2px turquoise, -2px -2px red";
-        selectionGrid.innerHTML = `
-        <a class="fade" id="f1"><i class="fa-solid fa-person" style="margin-right: 5px;"></i>Followers</a>
-        <a class="fade" id="f2"><i class="fa-brands fa-youtube" style="margin-right: 5px;"></i>Likes</a>
-        <a class="fade" id="f3"><i class="fa-brands fa-instagram" style="margin-right: 5px;"></i>Views</a>
-        <a class="fade" id="back">Back</a>
-        `;
+    function addBackButtonListener() {
         let back = selectionGrid.querySelector("#back");
         back.addEventListener("click", () => {
-            orignalState()
+            orignalState();
         });
+    }
 
-        selectionGrid.style.top = "75%";
-        selectionGrid.style.transform = "translate(-50%, -75%)";
-    });
+    function addServiceListeners() {
+        tiktokButton = document.querySelector("#tiktok");
+        youtubeButton = document.querySelector("#youtube");
+        instagram = document.querySelector("#instagram");
 
-    youtubeButton.addEventListener("click", () => {
+        tiktokButton.addEventListener("click", serviceClickHandler);
+        youtubeButton.addEventListener("click", serviceClickHandler);
+        instagram.addEventListener("click", serviceClickHandler);
+    }
+
+    function serviceClickHandler(event) {
+        let service = event.target.id;
         header.setAttribute("class","fade")
+        selectionGrid.style.top = "65%";
+        selectionGrid.style.transform = "translate(-50%, -65%)";
         header.innerHTML = `
-        <i class="fa-brands fa-youtube" style="font-size: 4vh;"></i>
-        <h1 style="margin-top: -10px">Youtube Service</h1>`;
+        <i class="fa-brands fa-${service}" style="font-size: 4vh"></i>
+        <h1 style="margin-top: -10px">${service.charAt(0).toUpperCase() + service.slice(1)} Service</h1>`;
         selectionGrid.innerHTML = `
         <a class="fade" id="f1"><i class="fa-solid fa-person" style="margin-right: 5px;"></i>Followers</a>
-        <a class="fade" id="f2"><i class="fa-brands fa-youtube" style="margin-right: 5px;"></i>Likes</a>
-        <a class="fade" id="f3"><i class="fa-brands fa-instagram" style="margin-right: 5px;"></i>Views</a>
-        <a class="fade" id="back">Back</a>
+        <a class="fade" id="f2"><i class="fa-solid fa-thumbs-up" style="margin-right: 5px;"></i>Likes</a>
+        <a class="fade" id="f3"><i class="fa-solid fa-eye" style="margin-right: 5px;"></i>Views</a>
+        <a class="fade" id="back"><i class="fa-solid fa-circle-xmark"></i></a>
         `;
+
+        let followers = selectionGrid.querySelector("#f1");
+        let likes = selectionGrid.querySelector("#f2");
+        let views = selectionGrid.querySelector("#f3");
         let back = selectionGrid.querySelector("#back");
-        back.addEventListener("click", () => {
-            orignalState()
+
+        [followers, likes, views].forEach((element) => {
+            element.addEventListener("click", () => {
+                selectionGrid.style.top = "55%";
+                selectionGrid.style.transform = "translate(-50%, -55%)";
+                let clickedElement = element.textContent;
+                header.textContent = "";
+                selectionGrid.innerHTML = `
+                <i class="fa-brands fa-${service} fade" style="font-size: 6vh;"></i>
+                <h1 id="header-title" class="fade" style="margin-top: 5px;">${service.charAt(0).toUpperCase() + service.slice(1)} ${clickedElement} </h1>
+                <p class="fade" style="margin-top:-18px;font-family: 'Inter';font-size:1.5vh;">Enter a link, Free Panel will provide a random value.</p>
+                <br>
+                <input placeholder="Enter Link" class="fade" id="${service}Input" name="link">
+                <br><br>
+                <button id="sButton" class="fade" style="bottom: 65px;">Submit</button>
+                <a class="fade" id="back"><i class="fa-solid fa-circle-xmark"></i></a>
+                `;
+                timer();
+                addBackButtonListener();
+            })
         });
 
-        selectionGrid.style.top = "75%";
-        selectionGrid.style.transform = "translate(-50%, -75%)";
-    });
-
-    instagram.addEventListener("click", () => {
-        header.setAttribute("class","fade")
-        header.innerHTML = `
-        <i class="fa-brands fa-instagram" style="font-size: 4vh"></i>
-        <h1 style="margin-top: -10px">Instagram Service</h1>`;
-        selectionGrid.innerHTML = `
-        <a class="fade" id="f1"><i class="fa-solid fa-person" style="margin-right: 5px;"></i>Followers</a>
-        <a class="fade" id="f2"><i class="fa-brands fa-youtube" style="margin-right: 5px;"></i>Likes</a>
-        <a class="fade" id="f3"><i class="fa-brands fa-instagram" style="margin-right: 5px;"></i>Views</a>
-        <a class="fade" id="back">Back</a>
-        `;
-        let back = selectionGrid.querySelector("#back");
         back.addEventListener("click", () => {
-            orignalState()
+            orignalState();
         });
+    }
 
-        selectionGrid.style.top = "75%";
-        selectionGrid.style.transform = "translate(-50%, -75%)";
-    });
+    addServiceListeners();
 });
